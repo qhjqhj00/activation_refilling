cd2340d6543d5f655b42f413fe1664630ac09f72
tommy <tommy@chien.io>
2025-05-29 17:33:59 +0800

    update

--- src/args.py
+++ src/args.py
@@ -229,3 +229,119 @@ class ModelArgs:
         if hasattr(self, "result_dir"):
             if self.result_dir is None: 
                 self.result_dir = "tmp"
+
+@dataclass
+class TrainingArgs(TrainingArguments):
+    # ==============================
+    # Common arguments
+    # ==============================
+    output_dir: str = field(
+        default="data/outputs/pretrain",
+    )
+
+    per_device_train_batch_size: int = field(
+        default=1,
+        metadata={'help': 'Train batch size.'}
+    )
+    per_device_eval_batch_size: int = field(
+        default=1,
+        metadata={'help': 'Evaluation batch size.'}
+    )
+    remove_unused_columns: bool = field(
+        default=False,
+        metadata={'help': 'Remove columns in the dataset that are not registered in the forward function?'}
+    )
+    ddp_find_unused_parameters: bool = field(
+        default=False,
+        metadata={'help': 'Find unusuable parameters?'}
+    )
+    # NOTE: essential to keep comuputation graph because we need gradients for beacon tokens
+    use_reentrant: Optional[bool] = field(
+        default=None,
+        metadata={'help': "Use reetrant in gradient checkpointing?"}
+    )
+    report_to: str = field(
+        default="none",
+        metadata={'help': 'Log results by external tools?'}
+    )
+
+    # ==============================
+    # Customized arguments
+    # ==============================
+    min_length: int = field(
+        default=0,
+        metadata={'help': 'How many tokens at minimum for training?'}
+    )
+
+    group_by_stride: Optional[str] = field(
+        default=None,
+        metadata={'help': 'Group the training data instances by the number of strides in the beacon model. {relaxed, strict}'}
+    )
+    sort_by_stride: Optional[str] = field(
+        default=None,
+        metadata={'help': 'Sort the training data instances by the number of strides in the beacon model. {ascend, descend}'}
+    )
+    only_train_beacon: bool = field(
+        default=True,
+        metadata={'help': 'Freeze LLM parameters when training beacon parameters?'}
+    )
+    
+    eval_method: str = field(
+        default="perplexity",
+        metadata={'help': 'How to evaluate during training? {perplexity, generation}'}
+    )
+    eval_max_length: int = field(
+        default=4096,
+        metadata={'help': 'How many tokens at maximum for each input in evaluation.'},
+    )
+    eval_min_length: int = field(
+        default=512,
+        metadata={'help': 'How many tokens at minimum for each input in evaluation.'},
+    )
+    eval_beacon_ratio: List[int] = field(
+        default_factory=lambda: [32],
+        metadata={'help': 'Condensing ratios for beacons in evaluation.'}
+    )
+    eval_beacon_ratio_mix: str = field(
+        default="adapt-1024",
+        metadata={'help': 'How to determine the beacon_ratio for each input. {step-random, instance-random, adapt-x}'}
+    )
+    max_eval_num: Optional[int] = field(
+        default=None,
+        metadata={'help': 'How many samples for validation?'}
+    )
+
+    lora_tune: bool = field(
+        default=False,
+        metadata={"help": "Use LoRA fine-tuning?"},
+    )
+    lora_rank: int = field(
+        default=32,
+        metadata={'help': 'LoRA rank.'}
+    )
+    lora_alpha: int = field(
+        default=16,
+        metadata={'help': 'LoRA scaling factor.'}
+    )
+    lora_dropout: float = field(
+        default=0.,
+        metadata={'help': 'LoRA dropout p.'}
+    )
+    lora_targets: List[str] = field(
+        default_factory=lambda: ["q_proj", "k_proj", "v_proj", "o_proj"],
+        metadata={"help": "Module name patterns to add LoRA."},
+    )
+    lora_extra_params: List[str] = field(
+        default_factory=lambda: ["embed_tokens", "norm"],
+        metadata={"help": "Extra trainable parameters except LoRA weights, if low rank training."},
+    )
+
+    metrics: List[str] = field(
+        default_factory=lambda: [],
+        metadata={'help': 'List of metrics. {rouge, save_result}'}
+    )
+    log_path: str = field(
+        default="data/outputs/metrics.log",
+        metadata={'help': 'Log file path.'}
+    )
+    
--- src/utils.py
+++ src/utils.py
@@ -1,170 +1,39 @@
-import pandas as pd
-import json
-from collections import defaultdict
-from statistics import quantiles
-from math import ceil
-from typing import Optional, List, Dict, Any, Mapping, Iterable
-from dataclasses import dataclass
-import shutil
-import torch
-import pathlib
-from tqdm import tqdm
+import os
 import sys
-import re
 import pytz
-from datetime import datetime
-from transformers.tokenization_utils import PreTrainedTokenizer
-from sacremoses import MosesTokenizer, MosesDetokenizer
-import tiktoken
-import nltk
-encoder = tiktoken.get_encoding("cl100k_base")
-import random
+import json
+import torch
+import shutil
+import pathlib
+import time
+import pickle
+import logging
+import string
 import numpy as np
+from contextlib import contextmanager
+from dataclasses import dataclass
+from transformers.tokenization_utils import PreTrainedTokenizer
+from datetime import datetime
+from collections import OrderedDict
+from typing import Optional, List, Dict, Any, Mapping, Iterable, Union
 
-def set_seed(seed: int):
-    """
-    Set a fixed seed for reproducibility across multiple libraries.
+try:
+    import paddle
+except:
+    pass
 
-    Parameters:
-    seed (int): The seed value to use for random number generation.
-    """
-    # Set the seed for Python's random module
-    random.seed(seed)
-    
-    # Set the seed for NumPy
-    np.random.seed(seed)
-    
-    # Set the seed for PyTorch
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU
-    
-    # Ensures deterministic behavior when using CUDA (may affect performance)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+logger = logging.getLogger(__name__)
 
-def extract_sentences(text, sentence_indices):
-    # Dictionary to store the sentences with their corresponding index
-    sentences_dict = {}
-    
-    # Regular expression to find sentences starting with [sX]
-    pattern = r'(\[s\d+\])\s*(.*?)(?=\[\s*s\d+\]|\Z)'
-    for match in re.finditer(pattern, text, re.DOTALL):
-        index = match.group(1)
-        sentence = match.group(2).strip()
-        sentences_dict[index] = sentence
-    print(sentences_dict)
-    # Retrieve the requested sentences based on the input list of indices
-    extracted_sentences = [sentences_dict.get(index) for index in sentence_indices if index in sentences_dict]
-    
-    return extracted_sentences
 
-def csv2json(file_path, sep=','):
-    df = pd.read_csv(file_path, sep=sep, engine='python')
-    json_data = df.to_dict(orient='records')
-    return json_data
+@contextmanager
+def do_nothing():
+    yield
 
-def json2csv(json_list, out_path):
-    df = pd.DataFrame(json_list)
-    df.to_csv(out_path, index=False)
-
-def load_jsonl(path: str) -> List:
-    rtn = []
-    print(f"Begin to load {path}")
-    for line in tqdm(open(path)):
-        line = json.loads(line)
-        rtn.append(line)
-    return rtn
-
-def save_jsonl(data: list, path: str) -> None:
-    with open(path, "w") as f:
-        for line in data:
-            f.write(
-                json.dumps(
-                    line, 
-                    ensure_ascii=False
-                    )+"\n")
-
-def load_json(path: str):
-    with open(path, "r") as f:
-        rtn = json.load(f)
-    return rtn
-
-def save_json(data, path: str):
-    with open(path, "w") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-
-def load_txt(path):
-    return open(path).read()
-
-def save_txt(content, path):
-    with open(path, "w") as f:
-        f.write(content, path)
-
-@dataclass
-class DefaultDataCollator:
-    """
-    Data collator that can:
-    1. Dynamically pad all inputs received. The inputs must be dict of lists.
-    2. Add position_ids based on attention_mask if required.
-    """
-    tokenizer: Optional[PreTrainedTokenizer] = None
-    padding_side: str = "left"
-    input_padding_value: int = 0
-    attention_padding_value: int = 0
-    label_padding_value: int = -100
-
-    keys_to_tensorize = {"input_ids", "attention_mask", "labels", "position_ids", "token_type_ids", "length", "depth", "index"}
-
-    def __call__(self, batch_elem: List) -> Dict[str, Any]:
-        first_elem = batch_elem[0]
-        return_batch = {}
-        
-        for key, value in first_elem.items():
-            # HACK: any key containing attention_mask must be attention_mask
-            # important to assign different pad token for different types of inputs
-            if "attention_mask" in key:
-                pad_token_id = self.attention_padding_value
-            elif "label" in key:
-                pad_token_id = self.label_padding_value
-            else:
-                pad_token_id = self.input_padding_value
-
-            batch_value = [elem[key] for elem in batch_elem]
-            # pad all lists and nested lists
-            if isinstance(value, list) and key in self.keys_to_tensorize:
-                max_length = get_max_length_in_nested_lists(batch_value)
-                batch_value, _ = pad_nested_lists(batch_value, max_length, pad_token_id, self.padding_side)
-
-            if key in self.keys_to_tensorize:
-                return_batch[key] = torch.tensor(batch_value)
-            else:
-                # handle strings and None
-                return_batch[key] = batch_value
-        return return_batch
-
-class FileLogger:
-    def __init__(self, log_file) -> None:
-        self.log_file = log_file
-    
-    def log(self, metrics, **kwargs):
-        with open(self.log_file, "a+") as f:
-            # get current time
-            tz = pytz.timezone('')
-            time = f"{'Time': <10}: {json.dumps(datetime.now(tz).strftime('%Y-%m-%d, %H:%M:%S'), ensure_ascii=False)}\n"
-            print(time)
-            command = f"{'Command': <10}: {json.dumps(' '.join(sys.argv), ensure_ascii=False)}\n"
-            print(command)
-            metrics = f"{'Metrics': <10}: {json.dumps(metrics, ensure_ascii=False)}\n"
-            msg = time + command
-
-            for key, value in kwargs.items():
-                x = f"{key: <10}: {json.dumps(value, ensure_ascii=False)}\n"
-                print(x)
-                msg += x
-            msg += metrics
-            print(metrics)
-            f.write(str(msg) + "\n")
+def optional_grad_ctx(with_grad=False):
+    if with_grad:
+        return do_nothing()
+    else:
+        return torch.no_grad()
 
 def makedirs(path):
     p = pathlib.Path(path)
@@ -202,3 +71,251 @@ def save_pickle(obj, path:str):
 def load_pickle(path):
     with open(path, "rb") as f:
         return pickle.load(f)
+    
+def save_json(obj, path:str):
+    if not os.path.exists(path):
+        makedirs(path)
+    with open(path, "w") as f:
+        return json.dump(obj, f)
+
+def load_json(path, lines=False):
+    if lines:
+        output = []
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                output.append(json.loads(line))
+        return output
+    else:
+        with open(path, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+def format_numel_str(numel: int) -> str:
+    T = 1e12
+    B = 1e9
+    M = 1e6
+    K = 1e3
+    if numel >= T:
+        return f"{numel / T:.2f} T"
+    if numel >= B:
+        return f"{numel / B:.2f} B"
+    elif numel >= M:
+        return f"{numel / M:.2f} M"
+    elif numel >= K:
+        return f"{numel / K:.2f} K"
+    else:
+        return f"{numel}"
+
+def batched_iter(iterable: Iterable, max_batch_size: int):
+    """ Batches an iterable into lists of given maximum size, yielding them one by one. """
+    batch = []
+    for element in iterable:
+        batch.append(element)
+        if len(batch) >= max_batch_size:
+            yield batch
+            batch = []
+    if len(batch) > 0:
+        yield batch
+
+def show_time(times):
+    times = np.array(times)
+    times = np.diff(times, axis=-1)
+    print(times)
+    return times
+
+@contextmanager
+def filelock(path, process_index=0):
+    while os.path.exists(path):
+        if i == 0 and process_index == 0:
+            logger.info("found lock, waiting for other programs...")
+        time.sleep(3)
+        i = 1
+    if process_index == 0:
+        save_json("this is a lock", path)
+    yield
+    if process_index == 0:
+        os.remove(path)
+
+def normalize_text(text, ignore_case=True, ignore_punctuation=True, ignore_space=True, ignore_number=False):
+    if isinstance(text, str):
+        text = [text]
+        unpack = True
+    else:
+        unpack = False
+    if ignore_case:
+        text = np.char.lower(text)
+    if ignore_punctuation:
+        repl_table = string.punctuation.maketrans("", "", string.punctuation)
+        text = np.char.translate(text, table=repl_table)
+    if ignore_number:
+        repl_table = string.digits.maketrans("", "", string.digits)
+        text = np.char.translate(text, table=repl_table)
+    if ignore_space:
+        for i, words in enumerate(np.char.split(text)):
+            text[i] = " ".join(words)
+    if isinstance(text, np.ndarray):
+        text = text.tolist()
+    if unpack:
+        text = text[0]
+    return text
+
+def wrap_text(s):
+    """Capitalize and add punctuation if there isn't."""
+    s = s.strip()
+    if not s[0].isupper():
+        s = s[0].capitalize() + s[1:]
+    if s[-1] not in string.punctuation:
+        s += "."
+    return s
+
+def min_max_normalize(array):
+    return (array - array.min(-1)[:,None])/(array.max(-1) - array.min(-1))[:, None]
+
+def softmax(x:np.ndarray, axis=-1):
+    if isinstance(x, list):
+        x = np.array(x)
+    x = x - x.max(axis=axis, keepdims=True)
+    y = np.exp(x)
+    return y / y.sum(axis=axis, keepdims=True)
+
+def get_max_length_in_nested_lists(lst):
+    if len(lst) and isinstance(lst[0], list):
+        lengths = []
+        for elem in lst:
+            length = get_max_length_in_nested_lists(elem)
+            lengths.append(length)
+        max_length = max(lengths)
+        return max_length
+    else:
+        return len(lst)
+
+def pad_nested_lists(lst, max_length, padding_value, padding_side="right"):
+    if isinstance(lst, list) and len(lst) and isinstance(lst[0], list):
+        masks = []
+        for i, elem in enumerate(lst):
+            lst[i], mask = pad_nested_lists(elem, max_length, padding_value, padding_side)
+            masks.append(mask)
+        return lst, masks
+    elif isinstance(lst, list):
+        if padding_side == "right":
+            mask = [1] * len(lst) + [0] * (max_length - len(lst))
+            lst = lst + [padding_value for _ in range(max_length - len(lst))]
+            return lst, mask
+        else:
+            mask = [0] * (max_length - len(lst)) + [1] * len(lst)
+            lst = [padding_value for _ in range(max_length - len(lst))] + lst
+            return lst, mask
+    else:
+        raise NotImplementedError(f"Unrecognized type {lst}")
+
+def mask_nested_lists(lst, mask_target, mask_value=0):
+    if isinstance(lst[0], list):
+        for i, elem in enumerate(lst):
+            lst[i] = mask_nested_lists(elem, mask_target, mask_value)
+        return lst
+    else:
+        return [x if x != mask_target else mask_value for x in lst]
+
+def are_elements_of_same_length(lst: List):
+    if not isinstance(lst[0], list):
+        return False
+
+    length = len(lst[0])
+    return all(len(x) == length if isinstance(x, list) else False for x in lst)
+
+def add_eos(inputs: Mapping, eos_token_id: int):
+    """Add eos for BatchEncoding object."""
+    assert isinstance(inputs["input_ids"], list), f"Make sure the return_tensors are set to list!"
+    if inputs["input_ids"][-1] != eos_token_id:
+        for k, v in inputs.items():
+            if k in ["input_ids", "labels"]:
+                v = v + [eos_token_id]
+            elif k == "attention_mask":
+                v = v + [1]
+            elif k == "position_ids":
+                v = v + [v[-1] + 1]
+            elif k == "token_type_ids":
+                v = v + v[-1:]
+            else:
+                raise NotImplementedError(f"Inputs key {k} not implemented!")
+            inputs[k] = v
+    return inputs
+
+def remove_eos(inputs: Mapping, eos_token_ids: Union[List,int]):
+    if isinstance(eos_token_ids, int):
+        eos_token_ids = [eos_token_ids]
+    input_ids = inputs["input_ids"]
+    eos_idx = [i for i, x in enumerate(input_ids) if x in eos_token_ids][0]
+    for k, v in inputs.items():
+        inputs[k].pop(eos_idx)
+    return inputs
+
+
+
+class FileLogger:
+    def __init__(self, log_file) -> None:
+        self.log_file = log_file
+    
+    def log(self, metrics, **kwargs):
+        with open(self.log_file, "a+") as f:
+            # get current time
+            tz = pytz.timezone('Asia/Shanghai')
+            time = f"{'Time': <10}: {json.dumps(datetime.now(tz).strftime('%Y-%m-%d, %H:%M:%S'), ensure_ascii=False)}\n"
+            print(time)
+            command = f"{'Command': <10}: {json.dumps(' '.join(sys.argv), ensure_ascii=False)}\n"
+            print(command)
+            metrics = f"{'Metrics': <10}: {json.dumps(metrics, ensure_ascii=False)}\n"
+            msg = time + command
+
+            for key, value in kwargs.items():
+                x = f"{key: <10}: {json.dumps(value, ensure_ascii=False)}\n"
+                print(x)
+                msg += x
+            msg += metrics
+            print(metrics)
+            f.write(str(msg) + "\n")
+
+
+@dataclass
+class DefaultDataCollator:
+    """
+    Data collator that can:
+    1. Dynamically pad all inputs received. The inputs must be dict of lists.
+    2. Add position_ids based on attention_mask if required.
+    """
+    tokenizer: PreTrainedTokenizer
+    attention_padding_value: int = 0
+    label_padding_value: int = -100
+
+    keys_to_tensorize = {"input_ids", "attention_mask", "labels", "position_ids", "token_type_ids", "length", "depth", "index"}
+
+    return_tensors: str = "pt"
+
+    def __call__(self, batch_elem: List) -> Dict[str, Any]:
+        first_elem = batch_elem[0]
+        return_batch = {}
+        
+        for key, value in first_elem.items():
+            # HACK: any key containing attention_mask must be attention_mask
+            # important to assign different pad token for different types of inputs
+            if "attention_mask" in key:
+                pad_token_id = self.attention_padding_value
+            elif "label" in key:
+                pad_token_id = self.label_padding_value
+            else:
+                pad_token_id = self.tokenizer.pad_token_id
+
+            batch_value = [elem[key] for elem in batch_elem]
+            # pad all lists and nested lists
+            if isinstance(value, list) and key in self.keys_to_tensorize:
+                max_length = get_max_length_in_nested_lists(batch_value)
+                batch_value, _ = pad_nested_lists(batch_value, max_length, pad_token_id, self.tokenizer.padding_side)
+
+            if key in self.keys_to_tensorize and None not in batch_value:
+                if self.return_tensors == "pt":
+                    return_batch[key] = torch.tensor(batch_value)
+                elif self.return_tensors == "pd":
+                    return_batch[key] = paddle.to_tensor(batch_value)
+            else:
+                # handle strings and None
+                return_batch[key] = batch_value
+        return return_batch
